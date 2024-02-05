d203320d70c5dad7759c2cff1b662f463e105e79
Julieta Agüero <julietaaguero021@gmail.com>
2024-02-05 18:17:26 -0300

    Settings

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -9,13 +9,44 @@ describe('Pokemon model', () => {
   describe('Validators', () => {
     beforeEach(() => Pokemon.sync({ force: true }));
     describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Pokemon.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
+      it('Should throw an error if name is null', async() => {
+        try{
+          await Pokemon.create({  
+          life: "36",
+          attack: "56",
+          defense: "41",
+          speed: "91",
+          height: "5",
+          weight: "61",
+          image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg",
+          types: [13, 6]})
+        }catch(err){
+          expect(err.message).to.equal(
+            "notNull Violation: Pokemon.name cannot be null",
+
+          );
+        }
+      
+     
       });
-      it('should work when its a valid name', () => {
-        Pokemon.create({ name: 'Pikachu' });
+      it('Should work when its a valid name', async () => {
+        await Pokemon.create( {
+          name: 'Pikachu',
+          life: "36",
+          attack: "56",
+          defense: "41",
+          speed: "91",
+          height: "5",
+          weight: "61",
+          image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg",
+          types: [13, 6]
+        });
+        const pokemon = await Pokemon.findOne({
+          where: {
+            name: 'Pikachu'
+          }
+        });
+        expect(pokemon.name).to.equal('Pikachu')
       });
     });
   });
--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -7,6 +7,14 @@ const { Pokemon, conn } = require('../../src/db.js');
 const agent = session(app);
 const pokemon = {
   name: 'Pikachu',
+  life: "36",
+  attack: "56",
+  defense: "41",
+  speed: "91",
+  height: "5",
+  weight: "61",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg",
+  types: [13, 6]
 };
 
 describe('Pokemon routes', () => {
@@ -19,6 +27,65 @@ describe('Pokemon routes', () => {
   describe('GET /pokemons', () => {
     it('should get 200', () =>
       agent.get('/pokemons').expect(200)
-    );
+    ).timeout(10000);
+    it('Responds a array whit 73 pokemons', async () => {
+      try {
+        const res = await agent.get('/pokemons');
+        expect(res.body).to.have.lengthOf(73);
+      } catch (error) {
+        console.log(error);
+      }
+    }).timeout(10000);
+    it('Respond with the pokemons that include that query', async () => {
+      try {
+        const res = await agent.get('/pokemons?name=bulbasaur');
+        expect(res.body[0].name).to.deep.include('bulbasaur');
+      } catch (error) {
+        console.log(error)
+      }
+    }).timeout(10000);
+    it('If an id parameter is passed invalid, respond with a 400', async () => {
+      try {
+        const res = await agent.get('/pokemons/1AS');
+        expect(res.body).res(400);
+      } catch (error) {
+      
+      }
+    })
   });
+  describe('GET /types', () => {
+    it('Should get types 200', async () => {
+      await agent.get('/types').expect(200)
+    }).timeout(10000);
+  })
+  
+  describe('POST /pokemons', () => {
+    it('Responds with 200', async () => {
+      try {
+        await agent.post('/pokemons').send({
+          name: "PIKACHU",
+          life: "36",
+          attack: "56",
+          defense: "41",
+          speed: "91",
+          height: "5",
+          weight: "61",
+          image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg",
+          types: [1, 13]
+        }).expect(200);
+      } catch (error) {
+        console.log(error)
+      }
+    });
+    it('If you dont pass params, respond with a 400', async () => {
+      try {
+        await agent.post('/pokemons').send({
+          name: "PIKACHU"
+        }).expect(400)
+      } catch (error) {
+        console.log(error)
+      }
+    })
+  })
 });
+
